5c2187696dee58a660d252141388b71051b85f86
moali <moali@uni.minerva.edu>
2023-12-13 06:20:10 +0800

    Mysql removed

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure--gqn8cxao&q&%n%68f56)_2=isyb@bfngt^%=9_$)(q97%dg2'
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['172.31.18.215','localhost','127.0.0.1','.vercel.app']
+ALLOWED_HOSTS = ['172.31.18.215','localhost','127.0.0.1','.vercel.app','now.sh']
 
 
 
