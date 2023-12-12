d00b8c919b1f40024693a184ea8ad69279f0bfd8
moali <moali@uni.minerva.edu>
2023-12-13 04:57:24 +0800

    More Changes

--- mysite/__init__.py
+++ mysite/__init__.py
@@ -1,3 +1,1 @@
-import pymysql
-
-pymysql.install_as_MySQLdb()
+from . import mysql_setup
--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -91,6 +93,10 @@ DATABASES = {
     }
 }
 
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default']['OPTIONS']['charset'] = 'utf8mb4'
+del DATABASES['default']['OPTIONS']['sslmode'] 
+DATABASES['default']['OPTIONS']['ssl'] =  {'ca': os.environ.get('MYSQL_ATTR_SSL_CA')}
 
 
 #User model
