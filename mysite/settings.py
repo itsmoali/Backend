d79d9f601ea3f81ab6a2bbe257203e1557417dc9
moali <moali@uni.minerva.edu>
2023-12-14 07:24:43 +0800

    CORS final update

--- mysite/settings.py
+++ mysite/settings.py
@@ -50,23 +50,16 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
 ]
 
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
-] 
-
-
-
-
+CORS_ALLOWED_ORIGINS = ['*']
 
 ROOT_URLCONF = 'mysite.urls'
 
