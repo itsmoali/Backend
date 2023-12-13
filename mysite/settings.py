6fadb84504eb6e1e4eef3d8fbdf62536c1e730da
moali <moali@uni.minerva.edu>
2023-12-14 07:28:38 +0800

    Cors settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = ['*']
+CORS_ALLOWED_ORIGINS = ['https://railway.vercel.app','https://railway.now.sh','http://localhost:3000','https://backend-swart-tau.vercel.app']
 
 ROOT_URLCONF = 'mysite.urls'
 
